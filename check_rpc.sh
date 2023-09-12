0e07e9216c8fa208c5e642193518dd2c9891e1d1
blockpi019 <127294963+blockpi019@users.noreply.github.com>
2023-09-12 10:52:00 +0800

    Update check_rpc.sh
    add

--- check_rpc.sh
+++ check_rpc.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-wget -q "https://filebrower.204001.xyz/api/raw/blockpi/software/evm-rpc-checker-linux?auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoyLCJsb2NhbGUiOiJ6aC1jbiIsInZpZXdNb2RlIjoibGlzdCIsInNpbmdsZUNsaWNrIjpmYWxzZSwicGVybSI6eyJhZG1pbiI6ZmFsc2UsImV4ZWN1dGUiOnRydWUsImNyZWF0ZSI6dHJ1ZSwicmVuYW1lIjp0cnVlLCJtb2RpZnkiOnRydWUsImRlbGV0ZSI6dHJ1ZSwic2hhcmUiOmZhbHNlLCJkb3dubG9hZCI6dHJ1ZX0sImNvbW1hbmRzIjpbXSwibG9ja1Bhc3N3b3JkIjp0cnVlLCJoaWRlRG90ZmlsZXMiOmZhbHNlLCJkYXRlRm9ybWF0IjpmYWxzZX0sImlzcyI6IkZpbGUgQnJvd3NlciIsImV4cCI6MTY5NDQ1MTIyMSwiaWF0IjoxNjk0NDQ0MDIxfQ.9l4Rif0l9BDYcTnaVnpOYQU-_DG5erZJSaYMxa5Jo94&" -O /root/evm-rpc-checker-linux
-chmod +x /root/evm-rpc-checker-linux
+if [ ! -f /root/evm-rpc-checker-linux ]; then
+    wget -q "http://file.204001.xyz/blockpi/software/evm-rpc-checker-linux" -O /root/evm-rpc-checker-linux
+    chmod +x /root/evm-rpc-checker-linux
+else
+    echo "/root/evm-rpc-checker-linux existed"
+fi
 
 cat /dev/null > /root/check_evp_rpc_host.log
 rpc_list=("127.0.0.1:21291" "127.0.0.1:21301" "127.0.0.1:31301" "127.0.0.1:21101")
 for rpc in "${rpc_list[@]}"
 do
     height=$(curl -Ls -X POST "http://$rpc" -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0","method": "eth_blockNumber","params": [],"id": 1}' | jq -r .result)
-    height_dec=$(echo "ibase=16;obase=10;${height#0x}" | bc)
     if [ ! -z $height ]; then
-		if [[ $height_dec > 0 ]]; then
-			echo "start check $rpc"
-			echo "block height: $height_dec"
-			/root/evm-rpc-checker-linux --url "$rpc" --check-tx-receipt >> /root/check_evp_rpc_host.log
-			cat /root/check_evp_rpc_host.log
-			echo "successful check $rpc"
-		fi
+		echo "start check $rpc"
+		echo "block height: $height_dec"
+		/root/evm-rpc-checker-linux --url "http://$rpc" --check-tx-receipt
+        #>> /root/check_evp_rpc_host.log
+		tail -n 9 /root/check_evp_rpc_host.log
+		echo "successful check $rpc"
     else
         echo "Skipping $rpc because it did not return a valid height"
     fi
